60a2e18578c1118564609af6e313732d979595ed
Bayer <bayer.tyurbeev@gmail.com>
2023-06-21 16:24:01 -0400

    update?

---
+++ src/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div>
+            <a href='#s'>Home</a>
+            <a href='#s'>News Feed</a>
+            <a href='#s'>Massages</a>
+        </div>
+    );
+}
+
+export default Header;
