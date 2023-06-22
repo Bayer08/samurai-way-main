b80a679ca36db69747e885a96aa34c438d332a23
Bayer <bayer.tyurbeev@gmail.com>
2023-06-22 14:49:45 -0400

    was added Header, Navbar, Profile components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
-// import Header from "./Header";
+import Header from "./Components/Header";
+import Navbar from "./Components/Navbar";
+import Profile from "./Components/Profile";
 // import Technologies from "./Technologies";
 // import Footer from "./Footer";
 
@@ -8,47 +10,9 @@ import './App.css';
 function App() {
     return (
         <div className='app-wrapper'>
-            <header className='header'>
-                <img
-                    src="https://png.pngtree.com/png-clipart/20190611/original/pngtree-wolf-logo-png-image_2306634.jpg"/>
-            </header>
-            <nav className='nav'>
-                <div><
-                    a>Profile</a>
-                </div>
-                <div>
-                    <a>Messages</a>
-                </div>
-                <div>
-                    <a>News</a>
-                </div>
-                <div>
-                    <a>Music</a>
-                </div>
-                <div>
-                    <a>Settings</a>
-                </div>
-
-            </nav>
-            <div className='content'>
-                <div>
-                    <img
-                        src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'/>
-                </div>
-                <div>
-                    ava+ discription
-                </div>
-                <div>
-                    My posts
-                    <div>
-                        New post
-                    </div>
-                    <div>
-                        <div>Post1</div>
-                        <div>Post2</div>
-                    </div>
-                </div>
-            </div>
+            <Header/>
+            <Navbar/>
+            <Profile />
         </div>
     );
 }
---
+++ src/Components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div className='header'>
+            <img
+                src="https://png.pngtree.com/png-clipart/20190611/original/pngtree-wolf-logo-png-image_2306634.jpg"/>
+        </div>
+    );
+}
+
+export default Header;
