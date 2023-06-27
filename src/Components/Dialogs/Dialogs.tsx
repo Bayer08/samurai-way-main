e0f78265b476032e15f60adf91c031b689cbc96b
Bayer <bayer.tyurbeev@gmail.com>
2023-06-27 16:26:35 -0400

    Dialog component page

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -3,8 +3,29 @@ import classes from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog+" "+classes.active}>
+                    Dima
+                </div>
+                <div className={classes.dialog}>
+                    Yura
+                </div>
+                <div className={classes.dialog}>
+                    Sveta
+                </div>
+                <div className={classes.dialog}>
+                    Egor
+                </div>
+                <div className={classes.dialog}>
+                    Roma
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>YO</div>
+                <div className={classes.message}>HI</div>
+                <div className={classes.message}>How are you?</div>
+            </div>
         </div>
     )
 }
