07c4fd3ae477108fd497dcccbb108fffadc2c4fb
Bayer <bayer.tyurbeev@gmail.com>
2023-06-27 16:41:11 -0400

    Lesson 22. EXact route.

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import classes from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
                 <div className={classes.dialog+" "+classes.active}>
-                    Dima
+                    <NavLink to="/dialog/1">Dima</NavLink>
                 </div>
                 <div className={classes.dialog}>
-                    Yura
+                    <NavLink to="/dialog/2">Yura</NavLink>
                 </div>
                 <div className={classes.dialog}>
-                    Sveta
+                    <NavLink to="/dialog/3">Sveta</NavLink>
                 </div>
                 <div className={classes.dialog}>
-                    Egor
+                    <NavLink to="/dialog/4">Egor</NavLink>
                 </div>
                 <div className={classes.dialog}>
-                    Roma
+                    <NavLink to="/dialog/5">Roma</NavLink>
                 </div>
             </div>
             <div className={classes.messages}>
