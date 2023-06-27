f2ba32b3d26ae47b670dbdbda058558303aad7fc
Bayer <bayer.tyurbeev@gmail.com>
2023-06-27 15:26:03 -0400

    added Routes Profile and Dialogs

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,26 @@ import Header from "./Components/Header/Header";
 import Navbar from "./Components/Navbar/Navbar";
 import Profile from "./Components/Profile/Profile";
 import Dialogs from "./Components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 // import Technologies from "./Technologies";
 // import Footer from "./Footer";
 
 
 function App() {
     return (
+        <BrowserRouter>
         <div className='app-wrapper'>
             <Header/>
             <Navbar/>
             <div className="content">
-                <Profile/>
-                {/*<Dialogs/>*/}
+                <Route path="/profile" component={Profile}/>
+                <Route path="/dialogs" component={Dialogs}/>
+                {/*<Route path="/news" component={News}/>*/}
+                {/*<Route path="/music" component={Music}/>*/}
+                {/*<Route path="/settings" component={Settings}/>*/}
             </div>
         </div>
+        </BrowserRouter>
     )
 }
 
